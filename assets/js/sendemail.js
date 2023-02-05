13a46e0a199277073f8129b37977092153fb8d88
ericjonesdev <ericjones2004@yahoo.com>
2023-02-05 14:38:23 +0000

    Add sendMail function

---
+++ assets/js/sendemail.js
@@ -0,0 +1,15 @@
+function sendMail(contactForm) {
+    emailjs.send("service_gutnj97", "template_6yxrk81", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        })
+    return false;
+}
